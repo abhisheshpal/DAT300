7570409e41f8314635c19221d7b3338d888d807f
Kristian Hovde Liland <khliland@users.noreply.github.com>
2019-09-03 11:12:48 +0200

    Intro and feature engineering beta 2

--- lectures/01/DAT300 information.py
+++ lectures/01/DAT300 information.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # ---
 # jupyter:
 #   jupytext:
@@ -19,6 +20,9 @@
 # + {"slideshow": {"slide_type": "-"}, "cell_type": "markdown"}
 # <img src="./images/DAT300_H2019.jpg" alt="DAT300" style="width: 736px;"/>
 
+# + {"slideshow": {"slide_type": "-"}, "cell_type": "markdown"}
+# - We are being watched (recorded)
+
 # + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
 # # The men
 # - Kristian Hovde Liland
@@ -26,12 +30,12 @@
 #   
 # Associate Professors in Data Science.
 
-# + {"slideshow": {"slide_type": "fragment"}, "cell_type": "markdown"}
-# ## The boys
-# - Runar Helin
-# - Stanislau "Stas" Trukhan
-# - Mike Riess  
-#   
+# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
+# # The boys
+# | Runar Helin              | Stanislau "Stas" Trukhan  |Mike Riess |  
+# |:-------------------------|:-------------------------:|----------:|  
+# | <img src="./images/runar.jpeg" alt="Runar" style="height: 250px;"/> | <img src="./images/stas.png" alt="Stas" style="height: 250px;"/>    | <img src="./images/mike.png" alt="Mike" style="height: 250px;"/>    |  
+#     
 # PhD students applying/developing machine- and deep learning methods.
 
 # + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
@@ -47,15 +51,43 @@
 #   - Compulsory assignments, competitions and solutions
 
 # + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
+# ## The tools
+# - Python 3.7: https://www.anaconda.com/
+# - Keras: https://keras.io/
+#   - Deep learning interface to Tensorflow et al.
+# - DASK: https://dask.org/
+#   - Out-of-core machine learning
+# - Jupytext: https://towardsdatascience.com/introducing-jupytext-9234fdff6c57
+#   - Jupyter Notebook extension for dual editing - Python code and Jupyter Notebook
+# - RISE slideshow: https://github.com/damianavila/RISE
+#   - Jupyter Notebook extension for presentation mode
+#
+
+# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
 # ## The book
 # <img src="./images/TheBook.jpg" alt="Python Machine Learning" style="width: 200px;"/>  
 # - Python Machine Learning, 2nd edition, By Raschka & Mirjalili  
 #     - Available in Boksmia
 #     - .. or https://www.akademika.no/python-machine-learning/raschka-sebastian/mirjalili-vahid/9781787125933
-#     - .. or https://www.packtpub.com/big-data-and-business-intelligence/python-machine-learning-second-edition
+#     - .. or https://www.packtpub.com/big-data-and-business-intelligence/python-machine-learning-second-edition (also 3rd edition)
 
 # + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
 # <img src="./images/TentativePlan_2019.png" alt="Tentative plan" style="width: 1032px;"/>
 
 # + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
 # <img src="./images/MarkZuckerberg.jpg" alt="Ethics" style="width: 720px;"/>
+
+# + {"slideshow": {"slide_type": "slide"}, "cell_type": "markdown"}
+# ## Expected student background
+# ### Programming (e.g. INF120 + INF200), Statistics (e.g. MATH-INF110/STAT100), Linear algebra (e.g. MATH113/MATH131)
+# ### Machine Learning (e.g. DAT200)
+# Python Machine Learning, Sebastian Raschka and Vahid Mirjalili, Packt Publishing
+# - Chapter 1 – Giving Computers the Ability to Lear from Data 
+# - Chapter 2 – Training Simple Machine Learning Algorithms for Classification
+# - Chapter 3 – A Tour of Machine Learning Classifiers
+# - Chapter 4 – Building Good Training Sets – Data Processing
+# - Chapter 5 – Compressing Data via Dimensionality Reduction
+# - Chapter 6 – Learning Best Practices for Model Evaluation and Hyperparamter Tuning 
+# - Chapter 7 – Combining Different Models for Ensemble Learning
+# - Chapter 10 – Predicting Continous Target Variables with Regression Analysis
+# - Chapter 11 – Working with unlabelled data – Clustering Analysis
