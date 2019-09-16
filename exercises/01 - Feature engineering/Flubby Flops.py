c58123f4877039cafa40c5fb83c237dece7af0ba
abhisheshpal <abhishesh.pal6@gmail.com>
2019-09-16 08:35:44 +0200

    first commit to DAT300

--- exercises/01 - Feature engineering/Flubby Flops.py
+++ exercises/01 - Feature engineering/Flubby Flops.py
@@ -34,3 +34,158 @@ Updated 2019.09.05
  
  Data are available as a CSV file with latin1 encoding.
  """
+
+# # Steps in Data Preprocessing
+# Step 1 : Import the libraries
+#
+# Step 2 : Import the data-set
+#
+# Step 3 : Check out the missing values
+#
+# Step 4 : See the Categorical Values
+#
+# Step 5 : Splitting the data-set into Training and Test Set
+#
+# Step 6 : Feature Scaling
+#
+#
+# ![ML.png](attachment:ML.png)
+# ![ml.jpeg](attachment:ml.jpeg)
+
+import numpy as np
+from numpy import nan
+import matplotlib.pyplot as plt
+from sklearn import preprocessing
+import pandas as pd
+from pandas import DataFrame
+import csv
+
+data = pd.read_csv('Flubby_Flops.csv', encoding = "ISO-8859-1")
+data.head()
+
+# +
+# check the missing value
+
+data.isnull().sum()    # seems 0 is missing in each column
+data.shape
+# mark all missing values
+#data.replace('?', nan, inplace=True)
+
+
+# +
+#data = pd.read_csv('Flubby_Flops.csv', header=0, low_memory=False, infer_datetime_format=True, parse_dates={'processstart':[0,1]}, index_col=['processstart'],encoding = "ISO-8859-1")
+
+#data.head()
+
+# +
+# convert feature 'process_start', 'boiling_stop', 'stretch_stop', timestamp into numeric
+
+#data_process = pd.DataFrame({'process_start': pd.date_range('2018-06-29 01:57:00', periods=987)})
+#data
+data['PS'] = pd.to_datetime(data.process_start)
+data['BS'] = pd.to_datetime(data.boiling_stop)
+data['SST'] = pd.to_datetime(data.stretch_stop)
+data
+
+
+# -
+
+data.drop(columns=['process_start', 'boiling_stop', 'stretch_stop'], axis=1, inplace= True)
+data.head()
+
+#Drop unnecesary features from data and decide the features
+data.drop(data.iloc[: , 0:1], axis=1, inplace= True)
+data.head()
+
+data.dtypes
+
+# replace catagorical features (floppiness) with binary valaues
+from sklearn.preprocessing import LabelEncoder
+
+label_enc = LabelEncoder()
+
+data.iloc[:,0] = label_enc.fit_transform(data.iloc[:,0])
+data
+
+dummy = pd.get_dummies(data.iloc[:,0])
+
+dummy.head()
+
+
+dummy.shape
+
+data2 = pd.concat([data, dummy])
+
+data2.head()
+
+data3 = data.join([dummy])
+
+data3.head()
+
+data3 = data3.drop(columns=['floppiness'])
+
+data3.head()
+
+
+# +
+# convert timestamp_to_float
+
+def convert_to_float(a):
+    return time.mktime(t.timetuple(a))
+
+
+# -
+
+from sklearn.linear_model import LinearRegression
+
+X = data3.iloc[:,:] #independent columns
+
+
+X = X.drop(columns=['flubberiness'])
+X.head()
+
+Y = data3.iloc[:,3] 
+
+   #target column i.e price range
+Y.head()
+
+import seaborn as sns
+#get correlations of each features in dataset
+corrmat = X.corr()
+top_corr_features = corrmat.index
+plt.figure(figsize=(10,8))
+#plot heat map
+g=sns.heatmap(X[top_corr_features].corr(),annot=True,cmap="RdYlGn")
+
+model = LinearRegression()
+model = model.fit(X,Y)
+
+r_sq = model.score(x, y)
+print('coefficient of determination:', r_sq)
+
+# dummy_1 = pd.get_dummies(data_1['floppiness'])
+
+
+# +
+#dummy_1
+
+# +
+#data_new = pd.concat([data_1, dummy_1], axis=1)
+
+# +
+#data_new
+
+# +
+#data.drop(data.iloc[:, 17:20], axis=1)
+
+# +
+#data
+# -
+
+
+
+
+
+
+
+data = pd.DataFrame(data_flubby)
--- exercises/01 - Feature engineering/Titanic Survival.py
+++ exercises/01 - Feature engineering/Titanic Survival.py
@@ -16,3 +16,110 @@ Test your Kaggle connection from Colab.
     - More ...
 - Make prediction of survival rate
 """
+
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import pandas as pd
+#import Dataframes as df
+#import utils as utils
+
+data_gender = pd.read_csv("gender_submission.csv") 
+data_gender.head()
+
+data_train = pd.read_csv("train.csv") 
+data_train.head()
+
+data_test = pd.read_csv("test.csv") 
+data_test.head()
+
+test_train = data_train.append(data_test, sort=False)
+test_train.head()
+
+test_train.columns(3)
+
+
+
+
+
+lebe = LabelEncoder()
+x_train = lebe.fit_transform(test_train)
+x_train.head()
+
+# +
+
+
+class MultiColumnLabelEncoder:
+    
+    def __init__(self, columns = None):
+
+        self.columns = columns # list of column to encode
+    
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, X):
+        '''
+        Transforms columns of X specified in self.columns using
+        LabelEncoder(). If no columns specified, transforms all
+        columns in X.
+        '''
+        
+        output = X.copy()
+        
+        if self.columns is not None:
+            for col in self.columns:
+                output[col] = LabelEncoder().fit_transform(output[col])
+        else:
+            for colname, col in output.iteritems():
+                output[colname] = LabelEncoder().fit_transform(col)
+        
+        return output
+    
+    def fit_transform(self, X, y=None):
+        return self.fit(X, y).transform(X)
+
+
+# -
+
+le = MultiColumnLabelEncoder()
+X_train_le = le.fit_transform(data_test)
+X_train_le.head()
+
+# +
+#fem = ['female']
+#mal = ['male']
+
+#enc = preprocessing.OneHotEncoder(categories=[fem, mal])
+# Note that for there are missing categorical values for the 2nd and 3rd
+# feature
+enc = preprocessing.OneHotEncoder()
+data_train = [['male'], ['female']]
+enc.fit(data_train) 
+
+OneHotEncoder(categorical_features='Sex',
+       categories=[...], drop=None,
+       dtype=np.float64, handle_unknown='error',
+       n_values=None, sparse=True)
+enc.transform([['male']]).toarray()
+# -
+
+enc.categories_
+
+data_train
+
+# +
+enc = preprocessing.OneHotEncoder()
+X = [['male'], ['female']]
+enc.fit(X)
+
+OneHotEncoder(categorical_features=None, categories=None, drop=None,dtype= np.float64, handle_unknown='error',
+       n_values=None, sparse=True)
+enc.transform([['male'],
+                ['female']]).toarray()
+# -
+
+enc.categories_
+
+full_data=train_data.append(test_data)
