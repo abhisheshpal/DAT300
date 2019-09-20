c789ab2f2293e1cb397b23b007fc277c2fb2385c
abhisheshpal <abhishesh.pal6@gmail.com>
2019-09-20 13:03:19 +0200

    new try fro sentiment analysis

--- exercises/02 - Sentiment analysis/IMDb_revisited.py
+++ exercises/02 - Sentiment analysis/IMDb_revisited.py
@@ -11,10 +11,8 @@ EXERCISE: Exchange Logistic regression with Naïve Bayes in sentiment analysis
    - Run the preprocessor
    - Load the Pickled object (gs_lr_tfidf) from file
 (2) Find the optimal combination of text processing from this object.
-(3) Create a pipeline with the text processing steps, but add a 
-(Multinomial) Naïve Bayes model at the end instead of logistic regression.
-NB! You can increase the speed and reduce the memory usage by limiting the number
-of words in the vocabulary in the TfidfVectorizer, e.g. by max_features=1000.
+(3) Create a pipeline with the text processing steps, but add a
+(Gaussian) Naïve Bayes model at the end instead of logistic regression.
    - Fit the model on the training data
    - Check performance on test data
 """
@@ -29,8 +27,8 @@ the results:
 
 What happens to the topics if you reduce the number of components to 3?
 
-Make an extreme case by reducing the vocabulary to a few words per topic, 
+Make an extreme case by reducing the vocabulary to a few words per topic,
 e.g. a vocabulary of 5 words x 3 topics = 15 (max_features). How does it
-affect the resulting categories, and can you still interpret the 
+affect the resulting categories, and can you still interpret the
 accompanying words?
 """
